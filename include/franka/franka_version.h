57374e69a01cc24854af4260d9c4e86929054883
GA <gaming-hacker@users.noreply.github.com>
2020-03-13 10:09:51 +0100

    add version header

---
+++ include/franka/franka_version.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/* Name of package */
+#define PACKAGE "libfranka"
+
+/* Version number of package */
+#define VERSION "0.71"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "support@franka.de"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libfranka 0.71"
+
+/* Define to the home page for this package. */
+#define PACKAGE_GIT "git://github.com/frankaemika/libfranka"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://frankaemika.github.io"
+
+/* Define license */
+#define LICENSE "Apache 2.0"
+
+
